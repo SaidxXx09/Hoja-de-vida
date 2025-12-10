f001b50874ae8efabe16ebf1e69144810c6abd08
Said_QuintoNevarez <quintonevarezariel@gmail.com>
2025-12-10 18:40:46 -0500

    Ejercicio completo

--- src/View/Frm_HojaDeVida.java
+++ src/View/Frm_HojaDeVida.java
@@ -20,7 +20,7 @@ public class Frm_HojaDeVida extends JFrame{
     private JLabel lbl_edad;
     private JLabel lbl_Cedula;
     private JLabel lbl_estadocivil;
-    private JComboBox comboBox_genero;
+    private JComboBox comboBox_estadoCivil;
     private JLabel lbl_genero;
     private JRadioButton rbtn_masculino;
     private JRadioButton rbtn_femenino;
@@ -45,6 +45,11 @@ public class Frm_HojaDeVida extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700,800);
         setLocationRelativeTo(null);
+        ButtonGroup genero = new ButtonGroup();
+        genero.add(rbtn_masculino);
+        genero.add(rbtn_femenino);
+        genero.add(rbtn_otro);
+        genero.clearSelection();
         btn_enviar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -54,30 +59,53 @@ public class Frm_HojaDeVida extends JFrame{
     }
 
     public void guardarArchivo() {
-        try {
-            FileWriter crear = new FileWriter("hoja_de_vida.txt",true);
+            String motivos = "";
+            if(txt_nombre.getText() == null || txt_nombre.getText().isEmpty()){
+                motivos += "Nombres vacios\n";
+            }
+            if(txt_apellido.getText() == null || txt_apellido.getText().isEmpty()){
+                motivos += "Apellidos vacios\n";
+            }
+            if(txt_edad.getText() == null || txt_edad.getText().isEmpty()){
+                motivos += "Edad vacia\n";
+            }
+            if(txt_cedula.getText() == null || txt_cedula.getText().isEmpty()){
+                motivos += "Cedula vacia\n";
+            }
+            if(txt_experiencia.getText() == null || txt_experiencia.getText().isEmpty()){
+                motivos += "Experiencia vacia\n";
+            }
+            if(txt_referencias.getText() == null || txt_referencias.getText().isEmpty()){
+                motivos += "Referencias vacias\n";
+            }
+            if (!chk_programador.isSelected() && !chk_despachador.isSelected()
+                    && !chk_recepcionista.isSelected() && !chk_secretario.isSelected()) {
+                motivos += "Debe seleccionar al menos un cargo\n";
+            }
+            if(!motivos.isEmpty()){
+                JOptionPane.showMessageDialog(null,"Error al guardar hoja de vida por los siguientes motivos: \n" + motivos);
+                return;
+            }
+
+            try{
+            FileWriter crear = new FileWriter("hoja_de_vida.txt");
             PrintWriter archivo = new PrintWriter(crear);
             archivo.println("=== HOJA DE VIDA ===");
             archivo.println("-- INFORMACION PERSONAL --");
-            String motivos = "";
-            if(txt_nombre == null || txt_nombre.isEm){
-                motivos += "Nombres vacios";
-            }
             archivo.println("Nombre: " + txt_nombre.getText());
             archivo.println("Apellido: " + txt_apellido.getText());
             archivo.println("Edad: " + txt_edad.getText());
             archivo.println("Cedula: " + txt_cedula.getText());
-            archivo.println("Cedula: " + txt_cedula.getText());
-            String sexo;
+            String sexo = "";
             if(rbtn_masculino.isSelected()){
                 sexo = "Masculino";
             } else if (rbtn_femenino.isSelected()) {
                 sexo = "Femenino";
-            } else {
+            } else if (rbtn_otro.isSelected()){
                 sexo = "Prefiero no decirlo";
             }
             archivo.println("Genero: " + sexo);
-            archivo.println("Estado civil: " + comboBox_genero.getSelectedItem());
+            archivo.println("Estado civil: " + comboBox_estadoCivil.getSelectedItem());
             archivo.println("Provincia: " + comboBox_provincias.getSelectedItem());
             archivo.println("-- INFORMACION LABORAL -- ");
             String cargo ="";
@@ -98,6 +126,21 @@ public class Frm_HojaDeVida extends JFrame{
             archivo.println("Referencias: " + txt_referencias.getText());
             archivo.close();
             JOptionPane.showMessageDialog(null,"Datos guardados con exito");
+            txt_nombre.setText("");
+            txt_apellido.setText("");
+            txt_referencias.setText("");
+            txt_experiencia.setText("");
+            txt_cedula.setText("");
+            txt_edad.setText("");
+            comboBox_provincias.setSelectedIndex(0);
+            comboBox_estadoCivil.setSelectedIndex(0);
+            rbtn_masculino.setSelected(false);
+            rbtn_femenino.setSelected(false);
+            rbtn_otro.setSelected(false);
+            chk_recepcionista.setSelected(false);
+            chk_programador.setSelected(false);
+            chk_secretario.setSelected(false);
+            chk_despachador.setSelected(false);
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null,"Error al guardar el archivo. \nMotivo/s:" + e.getMessage());
         }
